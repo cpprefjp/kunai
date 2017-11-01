69ee7acd3ef1cd6d367f7636f980acad7f0d43c6
melpon <shigemasa7watanabe+github@gmail.com>
2017-11-01 22:01:25 +0900

    Docker 周りのコードを修正
    - Dockerfile を docker/ の下に入れた
    - cron などのインタラクティブシェルが無い環境のために -it を付けなくできるようにした

--- docker.sh
+++ docker.sh
@@ -16,15 +16,19 @@ set -e
 
 cd "`dirname $0`"
 
+if [ -z "$DOCKER_IT" ] && [ "${DOCKER_IT:-A}" = "${DOCKER_IT-A}" ]; then
+  DOCKER_IT="-it"
+fi
+
 case "$1" in
-  "build" ) docker build -t kunai:0.0.0-alpine . ;;
-  "install" ) docker run -v `pwd`:/var/src -p 8080:8080 -it kunai:0.0.0-alpine /bin/sh -c "cd /var/src && exec npm install" ;;
+  "build" ) docker build -t kunai:0.0.0-alpine docker ;;
+  "install" ) docker run -v `pwd`:/var/src -p 8080:8080 $DOCKER_IT kunai:0.0.0-alpine /bin/sh -c "cd /var/src && exec npm install" ;;
   "run" )
     if [ $# -lt 2 ]; then
       show_help
       exit 1
     fi
-    docker run -v `pwd`:/var/src -p 8080:8080 -it kunai:0.0.0-alpine /bin/sh -c "cd /var/src && exec npm run $2" ;;
+    docker run -v `pwd`:/var/src -p 8080:8080 $DOCKER_IT kunai:0.0.0-alpine /bin/sh -c "cd /var/src && exec npm run $2" ;;
   * )
     show_help
     exit 1 ;;
