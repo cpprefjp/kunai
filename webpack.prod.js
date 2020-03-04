162a17a7f1d45b8e513c06154bb7829f594d37d0
melpon <shigemasa7watanabe+github@gmail.com>
2020-03-05 07:31:10 +0900

    clean-webpack-plugin を使ってはいけなかった
    js キー側にプラグインを設定してあるので、
    
    1. css キー側のファイルを生成
    2. js キー側の CleanWebpackPlugin 実行
    
    という順序で呼ばれると、生成した css や fonts が消えてしまう。

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const Merge = require('webpack-merge');
 
@@ -9,9 +8,6 @@ module.exports = env => (Merge.multiple(common(env), {
   js: {
     mode: 'production',
     plugins: [
-      new CleanWebpackPlugin(
-        { verbose: true, }
-      ),
       new webpack.DefinePlugin({
         'process.env': {
           'NODE_ENV': JSON.stringify('production')
