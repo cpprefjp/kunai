10ba73b5d6abdc637c45d2e791c4a77c49affdfb
nekko1119 <taku50501119@gmail.com>
2022-01-10 20:44:09 +0900

    imgが横幅を超える場合に横スクロールを可能にする

--- js/kunai/ui/content.js
+++ js/kunai/ui/content.js
@@ -7,6 +7,9 @@ class Content {
     this.log.debug('initialzing...')
 
     this.log.debug(`found ${Badge.sanitize($('main[role="main"] div[itemtype="http://schema.org/Article"] .content-body span.cpp'))} badges`)
+
+    // 横幅を超える画像を横スクロール可能にするためにスクロール用のdivで囲む
+    $('div[itemprop="articleBody"]').find('img').wrap('<div class="scrollable">')
   }
 }
 
